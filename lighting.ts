121740eba723f16b8eea0c63bd0d6c51eb431ccf
Jason Deacutis <jasondeacutis@gmail.com>
2018-07-16 17:59:15 +0000

    new vector methods, cleanup

--- lighting.ts
+++ lighting.ts
@@ -10,68 +10,58 @@
 */
 
 enum LIGHT_TYPE {
-	AMBIENT,
-	POINT,
-	DIRECTIONAL
+    AMBIENT,
+    POINT,
+    DIRECTIONAL
 }
 
 /*
 * Structure to hold lights
 */
 class Light {
-	constructor(
-		public type : LIGHT_TYPE,
-		public intensity : number,
-		public position? : Vector3,
-		public direction? : Vector3
-	) {}
+    constructor(
+        public type: LIGHT_TYPE,
+        public intensity: number,
+        public position?: Vector3,
+        public direction?: Vector3
+    ) { }
 }
 
 class Lighting {
-	public static CalcLighting(P : Vector3, N : Vector3, V : Vector3, lights : Light[], s : number, sphereArray : Sphere[]) : number {
-		let i : number = 0;
-		for(let l : number = 0;l < lights.length;l++) {
-			let li : Light = lights[l];
-			
-			if(li.type == LIGHT_TYPE.AMBIENT) {
-				i += li.intensity;
-			} else {
-				let L : Vector3 = undefined;
-				
-				if(li.type == LIGHT_TYPE.POINT) {
-					L = Math_3D.SubtractVectors(li.position, P);
-				} else {
-					L = li.direction;
-				}
-				
-				let new_l : Vector3 = Math_3D.AddVectors(L, N);
-				// shadows
-				let hit : Ray_Result = Math_3D.IntersectAllSpheres(P, new_l, sphereArray);
-				if(hit.sphere != undefined) {
-					continue;
-				}
-				
-				// diffuse lighting
-				let n_dot_l : number = Math_3D.DotProduct(N, L);
-				if(n_dot_l > 0) {
-					i += li.intensity * n_dot_l / (Math_3D.VectorMagnitude(N) * Math_3D.VectorMagnitude(L));
-				}
-				
-				// specular lighting
-				if(s != -1) {
-					let R1 : Vector3 = Math_3D.MultiplyVector(N, 2);
-					let R2 : number = Math_3D.DotProduct(N, L);
-					let R3 : Vector3 = Math_3D.MultiplyVector(R1, R2);
-					let R4 : Vector3 = Math_3D.SubtractVectors(R3, L);
-					
-					let r_dot_v : number = Math_3D.DotProduct(R4, V);
-					if(r_dot_v > 0) {
-						i += li.intensity * Math.pow(r_dot_v / (Math_3D.VectorMagnitude(R4) * Math_3D.VectorMagnitude(V)), s);
-					}
-				}
-			}
-		}
-		
-		return i;
-	}
+    public static CalcLighting(P: Vector3, N: Vector3, V: Vector3, lights: Light[], s: number, sphereArray: Sphere[]): number {
+        let i: number = 0;
+        for (let l: number = 0; l < lights.length; l++) {
+            let li: Light = lights[l];
+
+            if (li.type == LIGHT_TYPE.AMBIENT) {
+                i += li.intensity;
+            } else {
+                let L: Vector3 = (li.type == LIGHT_TYPE.POINT) ? li.position.Subtract(P) : li.direction;
+
+                // shadows
+                let hit: Ray_Result = Math_3D.IntersectAllSpheres(P, L.Add(N), sphereArray);
+                if (hit.sphere != undefined) {
+                    continue;
+                }
+
+                // diffuse lighting
+                let n_dot_l: number = N.Dot(L);
+                if (n_dot_l > 0) {
+                    i += li.intensity * n_dot_l / (N.Magnitude() * L.Magnitude());
+                }
+
+                // specular lighting
+                if (s != -1) {
+                    let R4: Vector3 = N.Multiply(2).Multiply(N.Dot(L)).Subtract(L);
+
+                    let r_dot_v: number = R4.Dot(V);
+                    if (r_dot_v > 0) {
+                        i += li.intensity * Math.pow(r_dot_v / (R4.Magnitude() * V.Magnitude()), s);
+                    }
+                }
+            }
+        }
+
+        return i;
+    }
 }
