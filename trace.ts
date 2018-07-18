768446fd1a2c5c4cd3a36735bd87196d4527f3e4
Jason Deacutis <jasondeacutis@gmail.com>
2018-07-17 20:09:20 -0400

    Render command, non-square res support

--- trace.ts
+++ trace.ts
@@ -3,109 +3,115 @@
 /// <reference path='math_3d.ts'/>
 /// <reference path='lighting.ts'/>
 
-// create the graphics context
-let canvas: any = document.getElementById("drawCanvas");
+function Render(resX: number = 256, resY: number = 256, samples: number = 0): void {
+	// create the graphics context
+	let canvas: any = document.getElementById("drawCanvas");
 
-let g: Graphics = new Graphics(canvas, 256, 256);
+	let g: Graphics = new Graphics(canvas, resX, resY);
 
-// define the spheres
-let sphereArray: Sphere[] = [
-    new Sphere(new Vector3(0, 1, 3), 1, new Vector3(255, 0, 0), 500),
-    new Sphere(new Vector3(2, 0, 6), 1, new Vector3(0, 0, 255), 500),
-    new Sphere(new Vector3(-2, 0, 4), 1, new Vector3(0, 255, 0), 10),
-    new Sphere(new Vector3(0, -5001, 0), 5000, new Vector3(255, 255, 0), 1000),
-];
+	// define the spheres
+	let sphereArray: Sphere[] = [
+		new Sphere(new Vector3(0, 1, 3), 1, new Vector3(255, 0, 0), 500),
+		new Sphere(new Vector3(2, 0, 6), 1, new Vector3(0, 0, 255), 500),
+		new Sphere(new Vector3(-2, 0, 4), 1, new Vector3(0, 255, 0), 10),
+		new Sphere(new Vector3(0, -5001, 0), 5000, new Vector3(255, 255, 0), 1000),
+	];
 
-// define the scene lights
-let lightArray: Light[] = [
-    new Light(LIGHT_TYPE.POINT, 0.6, new Vector3(2, 1, 0)),
-    new Light(LIGHT_TYPE.DIRECTIONAL, 0.2, undefined, new Vector3(1, 4, 4).Normalize()),
-    new Light(LIGHT_TYPE.AMBIENT, 0.2)
-];
+	// define the scene lights
+	let lightArray: Light[] = [
+		new Light(LIGHT_TYPE.POINT, 0.6, new Vector3(2, 1, 0)),
+		new Light(LIGHT_TYPE.DIRECTIONAL, 0.2, undefined, new Vector3(1, 4, 4).Normalize()),
+		new Light(LIGHT_TYPE.AMBIENT, 0.2)
+	];
 
-let backgroundColor = new Vector3(255, 255, 255);
+	let backgroundColor = new Vector3(255, 255, 255);
 
-// define camera pos and other stuff
-let camera: Vector3 = Vector3.Zero(); // camera is at the origin
+	// define camera pos and other stuff
+	let camera: Vector3 = Vector3.Zero(); // camera is at the origin
 
-// Multi Sample Anti Aliasing
-let MSAA_samples: number = 8;
+	// Multi Sample Anti Aliasing
+	let MSAA_samples: number = samples;
 
-let MSAA: Vector2[];
-if (MSAA_samples > 1) {// Randomly generate sample points within pixel
-    PRNG.seed = 46;
-    MSAA = Array(MSAA_samples);
-    for (let i: number = 0; i < MSAA_samples; i++) {
-        let max: Vector2;// Most distant sample from all other samples
-        let maxD: number = 0;
+	let MSAA: Vector2[];
+	if (MSAA_samples > 1) {// Randomly generate sample points within pixel
+		PRNG.seed = 46;
+		MSAA = Array(MSAA_samples);
+		for (let i: number = 0; i < MSAA_samples; i++) {
+			let max: Vector2;// Most distant sample from all other samples
+			let maxD: number = 0;
 
-        let tries: number = i == 0 ? 1 : 16;
-        for (let j: number = 0; j < tries; j++) {
-            //let cand: Vector2 = new Vector2(Math.random() - .5, Math.random() - .5);// random sample
-            let cand: Vector2 = new Vector2(PRNG.Random() - .5, PRNG.Random() - .5);// pseudo random sample
-            if (i == 0) {
-                max = cand;
-                continue;
-            }
-            else {
-                let minD: number = 10;
-                let min;
+			let tries: number = i == 0 ? 1 : 16;
+			for (let j: number = 0; j < tries; j++) {
+				//let cand: Vector2 = new Vector2(Math.random() - .5, Math.random() - .5);// random sample
+				let cand: Vector2 = new Vector2(PRNG.Random() - .5, PRNG.Random() - .5);// pseudo random sample
+				if (i == 0) {
+					max = cand;
+					continue;
+				}
+				else {
+					let minD: number = 10;
+					let min;
 
-                for (let k: number = 0; k < i; k++) {
-                    let offsets: Vector2[] = [
-                        new Vector2(-1, 1), new Vector2(0, 1), Vector2.One(),
-                        new Vector2(-1, 0), Vector2.Zero(), new Vector2(1, 0),
-                        new Vector2(-1, -1), new Vector2(0, -1), new Vector2(1, -1)
-                    ];
+					for (let k: number = 0; k < i; k++) {
+						let offsets: Vector2[] = [
+							new Vector2(-1, 1), new Vector2(0, 1), Vector2.One(),
+							new Vector2(-1, 0), Vector2.Zero(), new Vector2(1, 0),
+							new Vector2(-1, -1), new Vector2(0, -1), new Vector2(1, -1)
+						];
 
-                    let d: number = 10;
-                    for (let off of offsets) { d = Math.min(d, cand.Distance(MSAA[k].Add(off))); }
-                    if (d < minD) { minD = d; min = cand; }
-                }
+						let d: number = 10;
+						for (let off of offsets) { d = Math.min(d, cand.Distance(MSAA[k].Add(off))); }
+						if (d < minD) { minD = d; min = cand; }
+					}
 
-                if (minD > maxD) { maxD = minD; max = min; }
-            }
-        }
+					if (minD > maxD) { maxD = minD; max = min; }
+				}
+			}
 
-        MSAA[i] = max;
-        //console.log("samp" + (i + 1) + " " + Math.round(MSAA[i].x * 10) / 10 + " " + Math.round(MSAA[i].y * 10) / 10);
-    }
-} else if (MSAA_samples == -4) {// 4x MSAA
-    MSAA = [
-        new Vector2(-.25, .25),
-        new Vector2(.25, .25),
-        new Vector2(.25, -.25),
-        new Vector2(-.25, -.25)
-    ];
-} else { MSAA = [new Vector2(0, 0)]; }// No MSAA
+			MSAA[i] = max;
+			//console.log("samp" + (i + 1) + " " + Math.round(MSAA[i].x * 10) / 10 + " " + Math.round(MSAA[i].y * 10) / 10);
+		}
+	} else if (MSAA_samples == -4) {// 4x MSAA
+		MSAA = [
+			new Vector2(-.25, .25),
+			new Vector2(.25, .25),
+			new Vector2(.25, -.25),
+			new Vector2(-.25, -.25)
+		];
+	} else { MSAA = [new Vector2(0, 0)]; }// No MSAA
 
-if (MSAA_samples < 1) MSAA_samples = 1;
+	if (MSAA_samples < 1) MSAA_samples = 1;
 
-let intAllSphT: number = 0, CalcLightingT: number = 0;
-let sT: number = performance.now();
+	let intAllSphT: number = 0, CalcLightingT: number = 0;
+	let sT: number = performance.now();
 
-for (let x: number = -g.w * .5; x < g.w * .5; x++) {
-    for (let y: number = -g.h * .5; y < g.h * .5; y++) {
-        let c: Vector3 = Vector3.Zero();
+	let minResDim: number = Math.min(g.w, g.h);
 
-        for (let samp of MSAA) {
-            let n: Vector3 = new Vector3((x + samp.x) / g.w, (-y + samp.y) / g.h, 1).Normalize();
+	for (let x: number = -g.w * .5; x < g.w * .5; x++) {
+		for (let y: number = -g.h * .5; y < g.h * .5; y++) {
+			let c: Vector3 = Vector3.Zero();
 
-            let hit: Ray_Result = Math_3D.IntersectAllSpheres(camera, n, sphereArray);
+			for (let samp of MSAA) {
+				let n: Vector3 = new Vector3((x + samp.x) / minResDim, (-y + samp.y) / minResDim, 1).Normalize();
 
-            c = c.Add(
-                (hit.sphere != undefined) ?
-                    hit.sphere.color.Multiply(Lighting.CalcLighting(hit.hit_point, hit.hit_normal, n.Multiply(-1), lightArray, hit.sphere.specular, sphereArray))
-                    :
-                    backgroundColor
-            );
-        }
+				let hit: Ray_Result = Math_3D.IntersectAllSpheres(camera, n, sphereArray);
 
-        g.putPixel(new Vector2(x + g.w * .5, y + g.w * .5), c.Divide(MSAA_samples));
-    }
+				c = c.Add(
+					(hit.sphere != undefined) ?
+						hit.sphere.color.Multiply(Lighting.CalcLighting(hit.hit_point, hit.hit_normal, n.Multiply(-1), lightArray, hit.sphere.specular, sphereArray))
+						:
+						backgroundColor
+				);
+			}
+
+			g.putPixel(new Vector2(x + g.w * .5, y + g.h * .5), c.Divide(MSAA_samples));
+		}
+	}
+
+	g.showBuffer();
+
+	console.log("Total: " + Math.round(performance.now() - sT) * .001);
+	//console.log(g.w);
 }
 
-g.showBuffer();
-
-console.log("Total: " + Math.round(performance.now() - sT) * .001);
-//console.log(g.w);
+Render(256, 256, 8);
--- trace_gfx.ts
+++ trace_gfx.ts
@@ -6,134 +6,134 @@
 * Structure to hold a 2D vector
 */
 class Vector2 {
-    constructor(
-        public x: number,
-        public y: number
-    ) { }
+	constructor(
+		public x: number,
+		public y: number
+	) { }
 
-    public static Zero(): Vector2 { return new Vector2(0, 0); }
-    public static One(): Vector2 { return new Vector2(1, 1); }
+	public static Zero(): Vector2 { return new Vector2(0, 0); }
+	public static One(): Vector2 { return new Vector2(1, 1); }
 
-    public Add(V2: Vector2): Vector2 {
-        return new Vector2(this.x + V2.x, this.y + V2.y);
-    }
-    public Subtract(V2: Vector2): Vector2 {
-        return new Vector2(this.x - V2.x, this.y - V2.y);
-    }
-    public Magnitude(): number {
-        return Math.sqrt(this.x * this.x + this.y * this.y);
-    }
-    public Distance(V2: Vector2): number {
-        return this.Subtract(V2).Magnitude();
-    }
+	public Add(V2: Vector2): Vector2 {
+		return new Vector2(this.x + V2.x, this.y + V2.y);
+	}
+	public Subtract(V2: Vector2): Vector2 {
+		return new Vector2(this.x - V2.x, this.y - V2.y);
+	}
+	public Magnitude(): number {
+		return Math.sqrt(this.x * this.x + this.y * this.y);
+	}
+	public Distance(V2: Vector2): number {
+		return this.Subtract(V2).Magnitude();
+	}
 }
 
 /*
 * Structure to hold a 3D vector
 */
 class Vector3 {
-    constructor(
-        public x: number,
-        public y: number,
-        public z: number
-    ) { }
+	constructor(
+		public x: number,
+		public y: number,
+		public z: number
+	) { }
 
-    public static Zero(): Vector3 { return new Vector3(0, 0, 0); }
-    public static One(): Vector3 { return new Vector3(1, 1, 1); }
-    public static White(): Vector3 { return new Vector3(255, 255, 255); }
+	public static Zero(): Vector3 { return new Vector3(0, 0, 0); }
+	public static One(): Vector3 { return new Vector3(1, 1, 1); }
+	public static White(): Vector3 { return new Vector3(255, 255, 255); }
 
-    // Add vector by another
-    public Add(V2: Vector3): Vector3 {
-        return new Vector3(this.x + V2.x, this.y + V2.y, this.z + V2.z);
-    }
+	// Add vector by another
+	public Add(V2: Vector3): Vector3 {
+		return new Vector3(this.x + V2.x, this.y + V2.y, this.z + V2.z);
+	}
 
-    // Subtract vector by another
-    public Subtract(V2: Vector3): Vector3 {
-        return new Vector3(this.x - V2.x, this.y - V2.y, this.z - V2.z);
-    }
+	// Subtract vector by another
+	public Subtract(V2: Vector3): Vector3 {
+		return new Vector3(this.x - V2.x, this.y - V2.y, this.z - V2.z);
+	}
 
-    // Dot product of two vectors
-    public Dot(V2: Vector3): number {
-        return this.x * V2.x + this.y * V2.y + this.z * V2.z;
-    }
+	// Dot product of two vectors
+	public Dot(V2: Vector3): number {
+		return this.x * V2.x + this.y * V2.y + this.z * V2.z;
+	}
 
-    // Cross product of two vectors
-    public Cross(V2: Vector3): Vector3 {
-        let cx: number = this.y * V2.z - this.z * V2.y;
-        let cy: number = this.z * V2.x - this.x * V2.z;
-        let cz: number = this.x * V2.y - this.y * V2.x;
+	// Cross product of two vectors
+	public Cross(V2: Vector3): Vector3 {
+		let cx: number = this.y * V2.z - this.z * V2.y;
+		let cy: number = this.z * V2.x - this.x * V2.z;
+		let cz: number = this.x * V2.y - this.y * V2.x;
 
-        return new Vector3(cx, cy, cz);
-    }
+		return new Vector3(cx, cy, cz);
+	}
 
-    // Magnitude (Length) of vector
-    public Magnitude(): number {
-        return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
-    }
+	// Magnitude (Length) of vector
+	public Magnitude(): number {
+		return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+	}
 
-    // Vector multiplied by number
-    public Multiply(N: number): Vector3 {
-        return new Vector3(this.x * N, this.y * N, this.z * N);
-    }
+	// Vector multiplied by number
+	public Multiply(N: number): Vector3 {
+		return new Vector3(this.x * N, this.y * N, this.z * N);
+	}
 
-    // Vector multiplied by vector
-    public MultiplyVector(V: Vector3): Vector3 {
-        return new Vector3(this.x * V.x, this.y * V.y, this.z * V.z);
-    }
+	// Vector multiplied by vector
+	public MultiplyVector(V: Vector3): Vector3 {
+		return new Vector3(this.x * V.x, this.y * V.y, this.z * V.z);
+	}
 
-    // Vector divided by number
-    public Divide(N: number): Vector3 {
-        return new Vector3(this.x / N, this.y / N, this.z / N);
-    }
+	// Vector divided by number
+	public Divide(N: number): Vector3 {
+		return new Vector3(this.x / N, this.y / N, this.z / N);
+	}
 
-    // Unit (normalized) vector
-    public Normalize(): Vector3 {
-        return Math_3D.DivideVector(this, Math_3D.VectorMagnitude(this));
-    }
+	// Unit (normalized) vector
+	public Normalize(): Vector3 {
+		return Math_3D.DivideVector(this, Math_3D.VectorMagnitude(this));
+	}
 
-    // Absolute value
-    public Abs(): Vector3 {
-        return new Vector3(Math.abs(this.x), Math.abs(this.y), Math.abs(this.z));
-    }
+	// Absolute value
+	public Abs(): Vector3 {
+		return new Vector3(Math.abs(this.x), Math.abs(this.y), Math.abs(this.z));
+	}
 }
 
 /*
 * Class that controls graphical output
 */
 class Graphics {
-    private ctx: any;
-    private buf8: any;
-    private imageData: any;
+	private ctx: any;
+	private buf8: any;
+	private imageData: any;
 
-    constructor(canvas: any, public w: number, public h: number) {
-        canvas.width = w;
-        canvas.height = h;
-        this.ctx = canvas.getContext("2d");
-        this.imageData = this.ctx.getImageData(0, 0, w, h); // get the image data in the canvas
+	constructor(canvas: any, public w: number, public h: number) {
+		canvas.width = w;
+		canvas.height = h;
+		this.ctx = canvas.getContext("2d");
+		this.imageData = this.ctx.getImageData(0, 0, w, h); // get the image data in the canvas
 
-        // now put the bytes into an off-screen buffer
-        let buf: any = new ArrayBuffer(this.imageData.data.length);
-        this.buf8 = new Uint8ClampedArray(buf);
-    }
+		// now put the bytes into an off-screen buffer
+		let buf: any = new ArrayBuffer(this.imageData.data.length);
+		this.buf8 = new Uint8ClampedArray(buf);
+	}
 
 	/*
 	* Put a pixel into our int buffer
 	*/
-    putPixel(pos: Vector2, color: Vector3): void {
-        let offset: number = (pos.y * this.w + pos.x) * 4; // offset for pixels, 32-bit RGBA
+	putPixel(pos: Vector2, color: Vector3): void {
+		let offset: number = (pos.x + this.w * pos.y) * 4; // offset for pixels, 32-bit RGBA
 
-        this.buf8[offset] = color.x; // red
-        this.buf8[offset + 1] = color.y; // green
-        this.buf8[offset + 2] = color.z; // blue
-        this.buf8[offset + 3] = 255; // alpha
-    }
+		this.buf8[offset] = color.x; // red
+		this.buf8[offset + 1] = color.y; // green
+		this.buf8[offset + 2] = color.z; // blue
+		this.buf8[offset + 3] = 255; // alpha
+	}
 
 	/*
 	* Show the graphics buffer after drawing
 	*/
-    showBuffer(): void {
-        this.imageData.data.set(this.buf8);
+	showBuffer(): void {
+		this.imageData.data.set(this.buf8);
 
-        this.ctx.putImageData(this.imageData, 0, 0);
-    }
+		this.ctx.putImageData(this.imageData, 0, 0);
+	}
 }
